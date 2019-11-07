5fc3c2692214cc38c72b5cabecb0cb6b785381ff
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-11-07 02:56:08 -0800

    First 100 Fibonacci no importing function from that module and calculation of execution time

---
+++ Fibonacci 3.py
@@ -0,0 +1,10 @@
+"""First 100 Fibonacci no importing function from another module
+name: Suman Nandi
+Date: 06.11.2019"""
+
+from math import *
+from timeit import *
+from Fibonacci2 import f
+print ("First 100 Fibonacci no :",f(100))
+# time taken to run file:
+print('time taken to complete programme:  ',timeit())
